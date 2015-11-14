aa1cf237496b7a0bd19f16b5cc46935176fd6fe7
Shanmathi Krithivasan <mkshanmathi@gmail.com>
2015-11-13 21:02:00 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -140,7 +140,7 @@ var project = {
     "title": "MoonRunner iOS App",
     "url": "https://github.com/Shanmathi1?tab=repositories",
     "dates": "2015",
-    "description": "This is an iOS App developed using Swift, a GPS-based app to help you track your runs. This App takes running to the next level with badges based on planets and moons in the Solar System. This App uses the HealthKit Api introduced with iOS 8. It uses Core Location to track route, shows a map during the run, continously reports average pace, use Core Data to store past runs and awards badges for running various distances(Raywenderlich Tutorials)",
+    "description": "This is an iOS App developed using Swift, a GPS-based app to help you track your runs. This App takes running to the next level with badges based on planets and moons in the Solar System. This App uses the HealthKit Api introduced with iOS 8. It uses Core Location to track route, shows a map during the run, continously reports average pace, use Core Data to store past runs and awards badges for running various distances(Raywenderlich Tutorials).",
     "image": "images/ios1.png"
   },
   {
