40112007cd5af2e34275ff0c4bc3ee26d750bf41
Shanmathi Krithivasan <mkshanmathi@gmail.com>
2015-11-13 21:44:15 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -151,6 +151,13 @@ var project = {
     "image": "images/toDo1.png"
   },
   {
+    "title": "Stock Market Simulator",
+    "url": "https://github.com/Shanmathi1?tab=repositories",
+    "dates": "2015",
+    "description": "Stock Market Simulator is a Web Applications that allows the user experience all of the excitement of investing without the risk of losing their money! It was developed using Bootstrap framework, real time stock data was fetched from Yahoo! Finance APIs and user information was managed using MySQL. The users can learn the ins-and-outs of investing with Stock Market Trading Simulator and become the next mogul of Wall Street!",
+    "image": "images/stock1.png"
+  },
+  {
     "title": "Neighborhood Map",
     "url": "http://shanmathi1.github.io/neighborhood_map/",
     "dates": "2015",
